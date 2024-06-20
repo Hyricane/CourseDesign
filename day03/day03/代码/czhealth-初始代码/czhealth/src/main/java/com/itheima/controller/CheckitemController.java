9e7fcdd77cb176aa70b34a270cc3d3cce02cc80a
Hyricane <2825226055@qq.com>
2024-06-20 15:47:16 +0800

    cc

--- day03/day03/代码/czhealth-初始代码/czhealth/src/main/java/com/itheima/controller/CheckitemController.java
+++ day03/day03/代码/czhealth-初始代码/czhealth/src/main/java/com/itheima/controller/CheckitemController.java
@@ -2,12 +2,11 @@ package com.itheima.controller;
 
 import com.itheima.entity.PageResult;
 import com.itheima.entity.QueryPageBean;
+import com.itheima.entity.Result;
+import com.itheima.pojo.CheckItem;
 import com.itheima.service.CheckitemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/checkitem")
@@ -21,4 +20,22 @@ public class CheckitemController {
             return checkitemService.findPage(queryPageBean);
 
     }
+        @PostMapping("/add.do")
+        public Result add(@RequestBody CheckItem checkItem){
+            return checkitemService.add(checkItem);
+        }
+        @GetMapping("/delete.do")
+        public Result delete(Integer id){
+            return checkitemService.delete(id);
+        }
+
+        @GetMapping("/findById.do")
+        public Result findById(Integer id){
+            return checkitemService.findById(id);
+        }
+
+        @PostMapping("/edit.do")
+        public Result edit(@RequestBody CheckItem checkItem){
+            return checkitemService.edit(checkItem);
+        }
 }
--- day03/day03/代码/czhealth-初始代码/czhealth/src/main/java/com/itheima/service/CheckitemServiceImpl.java
+++ day03/day03/代码/czhealth-初始代码/czhealth/src/main/java/com/itheima/service/CheckitemServiceImpl.java
@@ -1,10 +1,13 @@
 package com.itheima.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.injector.methods.Insert;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.constant.MessageConstant;
 import com.itheima.entity.PageResult;
 import com.itheima.entity.QueryPageBean;
+import com.itheima.entity.Result;
 import com.itheima.mapper.CheckitemMapper;
 import com.itheima.pojo.CheckItem;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +29,7 @@ public class CheckitemServiceImpl implements CheckitemService{
 //        创建条件对象
         LambdaQueryWrapper<CheckItem> wrapper = new LambdaQueryWrapper<>();
         String queryString = queryPageBean.getQueryString();
-        if (queryPageBean !=null){
+        if (queryString !=null){
 //        若前端条件不为空 构建对应的 wrapper 对象
             wrapper.eq(CheckItem::getCode,queryString)
                     .or()
@@ -41,5 +44,35 @@ public class CheckitemServiceImpl implements CheckitemService{
 
 
         return new PageResult(selectPage.getTotal(),selectPage.getRecords());
+    }
+
+    @Override
+    public Result add(CheckItem checkItem) {
+        int insert = checkitemMapper.insert(checkItem);
+        if (insert <=0){
+            return new Result(false, MessageConstant.ADD_CHECKGROUP_FAIL);
+        }
+        return new Result(true,MessageConstant.ADD_CHECKGROUP_SUCCESS);
+    }
+
+    @Override
+    public Result delete(Integer id) {
+
+        return null;
+    }
+
+    @Override
+    public Result findById(Integer id) {
+        CheckItem checkItem = checkitemMapper.selectById(id);
+        if (checkItem == null){
+            return new Result(false,MessageConstant.QUERY_CHECKITEM_FAIL);
+        }
+        return  new Result(true,MessageConstant.QUERY_CHECKITEM_SUCCESS);
+    }
+
+    @Override
+    public Result edit(CheckItem checkItem) {
+        int update = checkitemMapper.updateById(checkItem)
+        return null;
     }
 }
